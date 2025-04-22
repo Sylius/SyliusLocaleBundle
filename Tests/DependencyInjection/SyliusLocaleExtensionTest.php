0741cc9dcac5e37ebf7a0349f5c4f0e523df34e5
Francis Hilaire <Prometee@users.noreply.github.com>
2025-04-22 14:59:34 +0200

    Import names

--- Tests/DependencyInjection/SyliusLocaleExtensionTest.php
+++ Tests/DependencyInjection/SyliusLocaleExtensionTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\LocaleBundle\Tests\DependencyInjection;
 
+use PHPUnit\Framework\Attributes\Test;
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 use Sylius\Bundle\LocaleBundle\Attribute\AsLocaleContext;
 use Sylius\Bundle\LocaleBundle\DependencyInjection\SyliusLocaleExtension;
@@ -21,7 +22,7 @@ use Symfony\Component\DependencyInjection\Definition;
 
 final class SyliusLocaleExtensionTest extends AbstractExtensionTestCase
 {
-    #[\PHPUnit\Framework\Attributes\Test]
+    #[Test]
     public function it_autoconfigures_locale_context_with_attribute(): void
     {
         $this->container->setDefinition(
